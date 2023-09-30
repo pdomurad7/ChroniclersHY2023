dbbc2ae744e9684f48f56020870008ac312179e6
pdomurad7 <domuradpawel01@gmail.com>
2023-10-01 00:04:05 +0200

    dupa

--- backend/app/ReportGenerator.py
+++ backend/app/ReportGenerator.py
@@ -1,16 +1,49 @@
 from app.cantors.CantorInterface import known_cantors
 import requests
+import datetime
 
 
 class ReportGenerator:
     def __init__(self, previous_report_data: dict):
+        self.report_header = {
+            "name": previous_report_data["name"],
+            "id": previous_report_data["id"],
+            "date": str(datetime.datetime.now().isoformat()),
+            "case_number": previous_report_data["case_number"],
+            "owner_data": previous_report_data["owner_data"],
+            "value_currency": previous_report_data["value_currency"]
+        }
         self.cryptocurrencies_amount = previous_report_data["cryptocurrencies_amount"]
         self.cryptocurrency_manual_rates = previous_report_data["cryptocurrency_manual_rates"]
+        self.nbp_usd_rate = self._get_nbp_usd_rate()
         self.cryptocurrencies_data = []
         self.exchange_data = []
-        self.nbp_usd_rate = self._get_nbp_usd_rate()
 
-    def known_cantors_rates(self):
+    def get_report(self):
+        self._update_exchange_data()
+        self._update_cryptocurrencies_data()
+        report = self.report_header
+        report["cryptocurrencies_data"] = self.cryptocurrencies_data
+        report["exchange_data"] = self.exchange_data
+        return report
+
+    def _update_exchange_data(self):
+        self._known_cantors_rates()
+        self._manual_cantors_rates()
+
+    def _update_cryptocurrencies_data(self):
+        for cryptocurrency in self.cryptocurrencies_amount:
+            cryptocurrency_dict = {"name": cryptocurrency["name"], "quantity": cryptocurrency["quantity"],
+                                   "data_sources": [], "avg_value": 0}
+            for cantor in self.exchange_data:
+                for rate in cantor["cryptocurrency_rates"]:
+                    if rate["code"] == cryptocurrency["name"]:
+                        cryptocurrency_dict["data_sources"].append(cantor["name"])
+                        cryptocurrency_dict["avg_value"] += rate["value"]
+            cryptocurrency_dict["avg_value"] /= len(cryptocurrency_dict["data_sources"])
+            self.cryptocurrencies_data.append(cryptocurrency_dict)
+
+    def _known_cantors_rates(self):
         for cantor in known_cantors:
             cantor_obj = cantor()
             cantor_dict = {"url": cantor_obj.url, "name": cantor_obj.name, "cryptocurrency_rates": []}
@@ -28,7 +61,23 @@ class ReportGenerator:
                     crypto_rate["code"] = cryptocurrency["name"]
                     crypto_rate["quantity"] = cryptocurrency["quantity"]
                     crypto_rate["value"] = float(crypto_rate["PLN_rate"]) * cryptocurrency["quantity"]
-                cantor_dict["cryptocurrency_rates"].append(crypto_rate)
+                if crypto_rate:
+                    cantor_dict["cryptocurrency_rates"].append(crypto_rate)
+            self.exchange_data.append(cantor_dict)
+
+    def _manual_cantors_rates(self):
+        for cantor in self.cryptocurrency_manual_rates:
+            cantor_dict = {"url": cantor["url"], "name": cantor["name"], "cryptocurrency_rates": []}
+            for cryptocurrency in self.cryptocurrencies_amount:
+                crypto_rate = {}
+                for rate in cantor["cryptocurrency_rates"]:
+                    if rate["name"] == cryptocurrency["name"]:
+                        crypto_rate["code"] = cryptocurrency["name"]
+                        crypto_rate["quantity"] = cryptocurrency["quantity"]
+                        crypto_rate["PLN_rate"] = rate["rate"]
+                        crypto_rate["value"] = float(crypto_rate["PLN_rate"]) * cryptocurrency["quantity"]
+                if crypto_rate:
+                    cantor_dict["cryptocurrency_rates"].append(crypto_rate)
             self.exchange_data.append(cantor_dict)
 
     @staticmethod
@@ -38,163 +87,3 @@ class ReportGenerator:
         for rate in response[0]["rates"]:
             if rate["code"] == "USD":
                 return rate["mid"]
-
-data = {
-    "name": "Crypto Asset Valuation",
-    "id": "12345",
-    "date": "09/30/2023",
-    "case_number": "ABC123",
-    "owner_data": "Jan Kowalski",
-    "value_currency": "PLN",
-    "calculation_method": "Average of Three Rates",
-    "cryptocurrencies_data": [
-        {
-            "name": "Bitcoin (BTC)",
-            "quantity": 0.001,
-            "data_sources": [
-                "cantor1",
-                "cantor2",
-            ],
-            "avg_value": 100,
-        },
-        {
-            "name": "Bitcoin (BTC)",
-            "quantity": 0.002,
-            "data_sources": [
-                "cantor1",
-                "cantor2",
-            ],
-            "avg_value": 110,
-        },
-        {
-            "name": "Ethereum (ETH)",
-            "quantity": 12,
-            "data_sources": [
-                "cantor1",
-                "cantor2",
-            ],
-            "avg_value": 11222,
-        },
-        {
-            "name": "Manchester City Fan Token (CITY)",
-            "quantity": 1000,
-            "data_sources": [
-                "cantor1",
-                "cantor2",
-            ],
-            "avg_value": 200330,
-        },
-    ],
-    "exchange_data": [
-        {
-            "url": "https://cantor-1.com",
-            "name": "cantor1",
-            "cryptocurrency_rates": [
-                {
-                    "code": "BTC",
-                    "quantity": 0.001,
-                    "USD_rate": 45000,
-                    "PLN_rate": 250000,
-                    "converted_from_USD": "true",
-                    "NBP_USD_rate": 4.33,
-                    "value": 36000,
-                },
-                {
-                    "code": "BTC",
-                    "quantity": 0.002,
-                    "USD_rate": 45000,
-                    "PLN_rate": 250000,
-                    "converted_from_USD": "true",
-                    "NBP_USD_rate": 4.33,
-                    "value": 36000,
-                },
-                {
-                    "code": "ETH",
-                    "quantity": 12,
-                    "USD_rate": 3000,
-                    "PLN_rate": 14000,
-                    "converted_from_USD": "true",
-                    "NBP_USD_rate": 4.33,
-                    "value": 15000,
-                },
-                {
-                    "code": "CITY",
-                    "quantity": 1000,
-                    "USD_rate": 1000,
-                    "PLN_rate": 4300,
-                    "converted_from_USD": "true",
-                    "NBP_USD_rate": 4.33,
-                    "value": 6000,
-                },
-            ],
-        },
-        {
-            "url": "https://cantor-2.com",
-            "name": "cantor2",
-            "cryptocurrency_rates": [
-                {
-                    "code": "BTC",
-                    "quantity": 0.001,
-                    "USD_rate": 45000,
-                    "PLN_rate": 250000,
-                    "converted_from_USD": "true",
-                    "value": 36000,
-                },
-                {
-                    "code": "BTC",
-                    "quantity": 0.002,
-                    "USD_rate": 45000,
-                    "PLN_rate": 250000,
-                    "converted_from_USD": "true",
-                    "value": 36000,
-                },
-                {
-                    "code": "ETH",
-                    "quantity": 12,
-                    "USD_rate": 3000,
-                    "PLN_rate": 14000,
-                    "converted_from_USD": "true",
-                    "value": 15000,
-                },
-                {
-                    "code": "CITY",
-                    "quantity": 1000,
-                    "USD_rate": 1000,
-                    "PLN_rate": 4300,
-                    "converted_from_USD": "true",
-                    "value": 6000,
-                },
-            ],
-        },
-        {
-            "url": "https://cantor-3.com",
-            "name": "cantor3",
-            "cryptocurrency_rates": [
-                {
-                    "code": "BTC",
-                    "quantity": 0.001,
-                    "USD_rate": None,
-                    "PLN_rate": None,
-                },
-                {
-                    "code": "BTC",
-                    "quantity": 0.002,
-                    "USD_rate": None,
-                    "PLN_rate": None,
-                },
-                {
-                    "code": "ETH",
-                    "quantity": 12,
-                    "USD_rate": None,
-                    "PLN_rate": None,
-                },
-                {
-                    "code": "CITY",
-                    "quantity": 1000,
-                    "USD_rate": None,
-                    "PLN_rate": None,
-                },
-            ],
-        },
-    ],
-}
