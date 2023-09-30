0e1b065745e2ec976d18d3b794a336642192927a
jkaminskii <din0mite31@gmail.com>
2023-09-30 16:21:16 +0200

    Add chief names list

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,15 @@
-from fastapi import FastAPI, Query, Path, HTTPException
+import json
+from fastapi import FastAPI
+from .config import settings
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.get("/chief-names")
+def list_chief_names():
+    with open(settings.CHIEF_NAMES_PATH, 'r', encoding='utf-8') as f:
+        return json.load(f)
